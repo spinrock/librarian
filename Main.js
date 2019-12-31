a46be07ad929b070a21bb1d8479c6407985698e4
Satoshi Fukai <chemistreal2@gmail.com>
2019-12-31 23:27:53 +0900

    週次出力/2週間分表示に変更

--- Main.js
+++ Main.js
@@ -6,18 +6,19 @@ function main(){
   var slack_username = 'Librarian';
   var slack_icon = ':book:';
   
+  const GETDURATIONDAYS = 14;
+
   var startDate = new Date();
-  startDate.setDate(1);
   var endDate = new Date(startDate);
-  endDate.setMonth(endDate.getMonth()+1);
+  endDate.setDate(endDate.getDate() + GETDURATIONDAYS);
   var postMessage;
 
   postMessage = getCalender(calendar_id,startDate, endDate);
     
   if (postMessage == "") {
-    postMessage = "今月の新刊情報はありません。"
+    postMessage = "新刊情報はありません。"
   } else {
-    postMessage = "今月の新刊一覧はこちら\n" + postMessage
+    postMessage = "新刊一覧はこちら\n" + postMessage
   }
   
   postToSlack(slack_url, slack_channel, slack_username, slack_icon, postMessage );
